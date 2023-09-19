58de96d0b4fd503dedddbbd4eab447e2d01858be
mmitusov <maksym.mitusov@gmail.com>
2023-09-19 16:34:54 +0300

    Sanity studio - data fetching

--- src/app/(user)/page.tsx
+++ src/app/(user)/page.tsx
@@ -1,15 +1,21 @@
+'use client'
+
 import BlogList from "@/components/BlogList";
 import { groq } from "next-sanity";
+import { client } from "../../../sanity-config/utils/sanity.client";
+
+const query = groq`
+  *[_type=='post'] {
+    ...,
+    author->,
+    categories[]->
+  } | order(_createdAt desc)
+`;
 
 export default function Home() {
-  const query = groq`
-    *[_type=='post'] {
-      ...,
-      author->,
-      categories[]->
-    } | order(_createdAt desc)
-  `;
-  
+  const posts = client.fetch(query)
+  console.log(posts)
+
   return (
     <main className="">
       <BlogList posts={posts}/>
