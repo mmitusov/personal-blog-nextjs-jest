73ef09a1c24f8ce47a5ba6bd7aa396f22383c810
mmitusov <maksym.mitusov@gmail.com>
2023-09-21 14:33:21 +0300

    BlogList bug fix

--- next.config.js
+++ next.config.js
@@ -1,4 +1,17 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  //If you want to display any images in nextjs app from accross the internet; here is my next config
+  images: {
+      domains: ['localhost'],
+      remotePatterns: [
+          {
+              protocol: 'https',
+              hostname: '**',
+              port: '',
+              pathname: '**',
+          },
+      ],
+  }
+}
 
 module.exports = nextConfig
--- src/components/BlogList.tsx
+++ src/components/BlogList.tsx
@@ -1,4 +1,8 @@
-import imgUrlSanity from "../../sanity-config/utils/imgURL";
+
+import Image from "next/image";
+import imgUrlSanity from "../../sanity-config/utils/imgUrl";
+import { HiArrowTopRightOnSquare } from "react-icons/hi2";
+
 
 interface BlogListProps {
   posts: Post[]; //Typed from typings.d.ts
@@ -8,51 +12,59 @@ const BlogList = ({posts}: BlogListProps) => {
   return (
     <div>
       <hr className="border-[#F7AB0A] mb-10"/>
-      <div>
+      {/* Grid */}
+      <div className="grid grid-cols-1 md:grid-cols-2 px-10 gap-10 gap-y-16 pb-24">
+        {/* Posts */}
         {posts.map((post) => (
-            <div key={post._id} className="flex flex-col group cursor-pointer">
-              <div className="relative w-full h-80 drop-shadow-xl group-hover:scale-105 transition-transform duration-200 ease-out">
-                <img 
-                  className="object-cover object-left lg:object-center"
-                  src={imgUrlSanity(post.mainImage).url()}
-                  alt={post?.author?.name ? post?.author?.name : 'Author not specified'}
-                />
+          <div key={post._id} className="flex flex-col group cursor-pointer">
+            <div className="relative w-full h-80 drop-shadow-xl group-hover:scale-105 transition-transform duration-200 ease-out">
+              <Image 
+                className="object-cover object-center"
+                src={imgUrlSanity(post.mainImage).url()}
+                alt={post?.author?.name ? post?.author?.name : 'Author not specified'}
+                fill
+              />
 
-                <div className="flex justify-between absolute bottom-0 w-full bg-black bg-opacity-20 backdrop-blur-lg text-white p-5">
-                  <div>
-                    <p className="font-bold">{post.title}</p>
-                    <p> 
-                      {new Date(post._createdAt).toLocaleDateString(
-                        "en-US", {
-                          day: "numeric", 
-                          month: "long",
-                          year: "numeric",
-                      })}
-                    </p>
-                  </div>
+              <div className="flex justify-between absolute bottom-0 w-full bg-black bg-opacity-20 backdrop-blur-lg text-white p-5">
+                <div>
+                  <p className="font-bold">{post.title}</p>
+                  <p> 
+                    {new Date(post._createdAt).toLocaleDateString(
+                      "en-US", {
+                        day: "numeric", 
+                        month: "long",
+                        year: "numeric",
+                    })}
+                  </p>
+                </div>
 
-                  <div className="flex flex-col items-center gap-y-2 md:flex-row md:gap-x-2">
-                    {post?.categories 
-                      ? 
-                        post?.categories?.map(category => (
-                          <div className="bg-[#F7AB0A] text-center text-black px-3 py-1 rounded-full text-sm font-semibold">
-                            <p>{category?.title ? category?.title : "No categories provided"}</p>
-                          </div>
-                        ))
-                      : 
-                        <div className="bg-[#aa61e2] text-center text-black px-3 py-1 rounded-full text-sm font-semibold">
-                          <p>No categories provided</p>
+                <div className="flex flex-col items-center gap-y-2 md:flex-row md:gap-x-2">
+                  {post?.categories 
+                    ? 
+                      post?.categories?.map(category => (
+                        <div className="bg-[#F7AB0A] text-center text-black px-3 py-1 rounded-full text-sm font-semibold">
+                          <p>{category?.title ? category?.title : "No categories provided"}</p>
                         </div>
-                    }
-                  </div>
+                      ))
+                    : 
+                      <div className="bg-[#aa61e2] text-center text-black px-3 py-1 rounded-full text-sm font-semibold">
+                        <p>No categories provided</p>
+                      </div>
+                  }
                 </div>
               </div>
-
-              <div className="mt-5 flex-1">
-                <p className="underline text-lg font-bold line-clamp-2">{post.title}</p>
-                <p className="text-gray-500">{post.description}</p>
-              </div>
             </div>
+
+            <div className="mt-5 flex-1">
+              <p className="underline text-lg font-bold line-clamp-2">{post.title}</p>
+              <p className="text-gray-500">{post.description}</p>
+            </div>
+
+            <p className="flex items-center font-bold hover:underline">
+              Read post
+              <HiArrowTopRightOnSquare className="h-4 w-4 ml-2"/>
+            </p>
+          </div>
         ))}
       </div>
     </div>
