8c6d81229abb3add45a5d35d6ad420e07e274ccb
mmitusov <maksym.mitusov@gmail.com>
2023-09-21 22:04:11 +0300

    Sanity groq query

--- src/app/(user)/post/[slug]/page.tsx
+++ src/app/(user)/post/[slug]/page.tsx
@@ -1,10 +1,23 @@
+import { groq } from "next-sanity";
+import { client } from "../../../../../sanity-config/utils/sanity.client";
+
 interface Props {
   params: {
     slug: string;
   }
 }
 
-const Post = ({params: {slug}}: Props) => {
+const Post = async ({params: {slug}}: Props) => {
+  const query = groq`
+    *[_type=='post' && slug.current == $slug][0] {
+      ...,
+      author->,
+      categories[]->
+    }
+  `;
+  const post: Post = await client.fetch(query, {slug})
+  console.log(post)
+
   return (
     <div>Post: {slug}</div>
   )
